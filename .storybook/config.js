4f9fdd232ad7580d050c052ce58bb7f8fc31517c
Viktor Bulvarenko <viktor.bulvarenko@gmail.com>
2019-12-15 15:51:00 +0200

    off themoing

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,11 +1,11 @@
 import { configure } from '@storybook/react';
-import { themes } from '@storybook/theming';
+// import { themes } from '@storybook/theming';
 
-addParameters({
-  options: {
-    theme: themes.dark,
-  },
-});
+// addParameters({
+//   options: {
+//     theme: themes.dark,
+//   },
+// });
 
 configure(require.context('../src', true, /\.stories\.js$/), module);
 
